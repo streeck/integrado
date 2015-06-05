eeae014639915b86f854c294fef9de5607fac740
Charles David <raverblazer@gmail.com>
2015-06-04 21:02:25 -0300

    Add Connection Factory and DAOException. Add files and packages as well.

---
+++ src/main/java/model/Fonte.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author charles
+ */
+public class Fonte {
+    private String fonte;
+    private double valor;
+    private String tipoLicitacao;
+
+    public String getFonte() {
+        return fonte;
+    }
+
+    public void setFonte(String fonte) {
+        this.fonte = fonte;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTipoLicitacao() {
+        return tipoLicitacao;
+    }
+
+    public void setTipoLicitacao(String tipoLicitacao) {
+        this.tipoLicitacao = tipoLicitacao;
+    }
+    
+    
+}
---
+++ src/main/java/model/SubDominio.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author charles
+ */
+public class SubDominio {
+    private String descricao;
+    private double valor;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+}
